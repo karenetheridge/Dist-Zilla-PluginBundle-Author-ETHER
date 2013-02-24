6de254dfb87ed7e40c20576bf0b36a73db4f30d4
Karen Etheridge <ether@cpan.org>
2013-02-23 17:54:41 -0800

    minting profile for repos hosted on github

---
+++ profiles/github/Module.pm
@@ -0,0 +1,40 @@
+package {{$name}};
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+...
+
+=head1 METHODS
+
+=over
+
+=item * C<foo>
+
+=back
+
+...
+
+=head1 SUPPORT
+
+Bugs may be submitted through L<https://rt.cpan.org/Public/Dist/Display.html?Name={{ $DIST }} >.
+I am also usually active on irc, as 'ether' at L<irc://irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+...
+
+=cut

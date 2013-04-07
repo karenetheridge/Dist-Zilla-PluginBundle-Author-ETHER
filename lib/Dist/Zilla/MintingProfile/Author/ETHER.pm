39d4c081f842ad8003d768adb027e60eca664675
Karen Etheridge <ether@cpan.org>
2013-04-07 11:34:46 -0700

    code and docs for minting profile

--- lib/Dist/Zilla/MintingProfile/Author/ETHER.pm
+++ lib/Dist/Zilla/MintingProfile/Author/ETHER.pm
@@ -1,9 +1,12 @@
 use strict;
 use warnings;
 package Dist::Zilla::MintingProfile::Author::ETHER;
-# ABSTRACT: ...
+# ABSTRACT: Mint distributions like ETHER does
 
+use Moose;
+with 'Dist::Zilla::Role::MintingProfile::ShareDir';
 
+__PACKAGE__->meta->make_immutable;
 1;
 __END__
 
@@ -11,33 +14,28 @@ __END__
 
 =head1 SYNOPSIS
 
-...
+dzil new -P Author::ETHER New::Module
 
 =head1 DESCRIPTION
 
-
-=head1 FUNCTIONS/METHODS
+Profiles available are:
 
 =begin :list
 
-* C<foo>
+* C<github>
+
+Creates a distribution hosted on L<http://github>.
+
+* C<default>
+
+Maps to C<github>.
 
 =end :list
-
-...
 
 =head1 SUPPORT
 
 Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-PluginBundle-Author-ETHER>
 (or L<mailto:bug-Dist-Zilla-PluginBundle-Author-ETHER@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
-
-=head1 ACKNOWLEDGEMENTS
-
-...
-
-=head1 SEE ALSO
-
-...
 
 =cut

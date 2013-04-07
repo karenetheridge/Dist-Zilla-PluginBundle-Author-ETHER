1c4fdf1af03f8dfa1c5b6355401a27ab519b9460
Karen Etheridge <ether@cpan.org>
2013-04-07 11:25:59 -0700

    skeleton for Dist::Zilla::MintingProfile::Author::ETHER

---
+++ lib/Dist/Zilla/MintingProfile/Author/ETHER.pm
@@ -0,0 +1,43 @@
+use strict;
+use warnings;
+package Dist::Zilla::MintingProfile::Author::ETHER;
+# ABSTRACT: ...
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+
+=head1 FUNCTIONS/METHODS
+
+=begin :list
+
+* C<foo>
+
+=end :list
+
+...
+
+=head1 SUPPORT
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-PluginBundle-Author-ETHER>
+(or L<mailto:bug-Dist-Zilla-PluginBundle-Author-ETHER@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+...
+
+=cut

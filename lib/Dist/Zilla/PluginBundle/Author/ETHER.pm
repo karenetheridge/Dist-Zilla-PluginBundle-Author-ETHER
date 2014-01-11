0aa0bedc559ea96299e79480d84e78a7a552af91
Karen Etheridge <ether@cpan.org>
2014-01-11 13:27:22 -0800

    experimental: use replace_with_comment in [PodWeaver]

--- lib/Dist/Zilla/PluginBundle/Author/ETHER.pm
+++ lib/Dist/Zilla/PluginBundle/Author/ETHER.pm
@@ -149,7 +149,7 @@ sub configure
         'Git::Describe',
         [ PkgVersion            => { ':version' => '5.010', die_on_existing_version => 1, die_on_line_insertion => 1 } ],
         [ 'Authority'           => { authority => 'cpan:ETHER' } ],
-        [ PodWeaver             => { ':version' => '4.005' } ],
+        [ PodWeaver             => { ':version' => '4.005', replacer => 'replace_with_comment', post_code_replacer => 'replace_with_nothing' } ],
         [ 'NextRelease'         => { ':version' => '4.300018', time_zone => 'UTC', format => '%-8v  %{yyyy-MM-dd HH:mm:ss\'Z\'}d%{ (TRIAL RELEASE)}T' } ],
         [ 'ReadmeAnyFromPod'    => { type => 'markdown', filename => 'README.md', location => 'build' } ],
 
@@ -371,6 +371,8 @@ following F<dist.ini> (following the preamble):
 
     [PodWeaver]
     :version = 4.005
+    replacer = replace_with_comment
+    post_code_replacer = replace_with_nothing
 
     [NextRelease]
     :version = 4.300018

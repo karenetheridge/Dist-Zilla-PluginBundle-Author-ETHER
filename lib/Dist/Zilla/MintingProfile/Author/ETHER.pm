e4bff8e25b0321296953b2c453eaaa7357cabeb9
Karen Etheridge <ether@cpan.org>
2015-03-05 20:36:30 -0800

    increment $VERSION after release

--- lib/Dist/Zilla/MintingProfile/Author/ETHER.pm
+++ lib/Dist/Zilla/MintingProfile/Author/ETHER.pm
@@ -4,7 +4,7 @@ package Dist::Zilla::MintingProfile::Author::ETHER;
 # ABSTRACT: Mint distributions like ETHER does
 # vim: set ts=8 sw=4 tw=78 et :
 
-our $VERSION = '0.089';
+our $VERSION = '0.090';
 
 use Moose;
 with 'Dist::Zilla::Role::MintingProfile';
--- lib/Dist/Zilla/PluginBundle/Author/ETHER.pm
+++ lib/Dist/Zilla/PluginBundle/Author/ETHER.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::PluginBundle::Author::ETHER;
 # KEYWORDS: author bundle distribution tool
 # vim: set ts=8 sw=4 tw=78 et :
 
-our $VERSION = '0.089';
+our $VERSION = '0.090';
 
 use Moose;
 with

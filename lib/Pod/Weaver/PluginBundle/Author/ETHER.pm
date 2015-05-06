b204f22df05ca1dbc1e488c5faa7b954702f4aac
Karen Etheridge <ether@cpan.org>
2015-05-05 21:45:02 -0700

    newly-minted Pod::Weaver::PluginBundle::Author::ETHER

---
+++ lib/Pod/Weaver/PluginBundle/Author/ETHER.pm
@@ -0,0 +1,48 @@
+use strict;
+use warnings;
+package Pod::Weaver::PluginBundle::Author::ETHER;
+# ABSTRACT: ...
+# KEYWORDS: ...
+# vim: set ts=8 sts=4 sw=4 tw=78 et :
+
+our $VERSION = '0.001';
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Pod::Weaver::PluginBundle::Author::ETHER;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=head2 C<foo>
+
+...
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-PluginBundle-Author-ETHER>
+(or L<bug-Dist-Zilla-PluginBundle-Author-ETHER@rt.cpan.org|mailto:bug-Dist-Zilla-PluginBundle-Author-ETHER@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=for :list
+* L<foo>
+
+=cut

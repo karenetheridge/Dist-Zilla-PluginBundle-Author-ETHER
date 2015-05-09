edee4e86cba6df54d50c6c6e9db46acb1eab611e
Karen Etheridge <ether@cpan.org>
2015-05-09 14:08:13 -0700

    first, make my weaver bundle act like [@Default]

--- lib/Pod/Weaver/PluginBundle/Author/ETHER.pm
+++ lib/Pod/Weaver/PluginBundle/Author/ETHER.pm
@@ -1,11 +1,40 @@
 use strict;
 use warnings;
 package Pod::Weaver::PluginBundle::Author::ETHER;
-# ABSTRACT: ...
-# KEYWORDS: ...
+# ABSTRACT: A plugin bundle for pod woven by ETHER
 # vim: set ts=8 sts=4 sw=4 tw=78 et :
 
 our $VERSION = '0.001';
+
+use namespace::autoclean;
+
+use Pod::Weaver::Config::Assembler;
+sub _exp { Pod::Weaver::Config::Assembler->expand_package($_[0]) }
+
+sub mvp_bundle_config {
+  return (
+    [ '@Author::ETHER/CorePrep',        _exp('@CorePrep'), {} ],
+    [ '@Author::ETHER/SingleEncoding',  _exp('-SingleEncoding'), {} ],
+    [ '@Author::ETHER/Name',            _exp('Name'),      {} ],
+    [ '@Author::ETHER/Version',         _exp('Version'),   {} ],
+
+    [ '@Author::ETHER/prelude',         _exp('Region'),    { region_name => 'prelude'  } ],
+    [ 'SYNOPSIS',                       _exp('Generic'),   {} ],
+    [ 'DESCRIPTION',                    _exp('Generic'),   {} ],
+    [ 'OVERVIEW',                       _exp('Generic'),   {} ],
+
+    [ 'ATTRIBUTES',                     _exp('Collect'),   { command => 'attr'   } ],
+    [ 'METHODS',                        _exp('Collect'),   { command => 'method' } ],
+    [ 'FUNCTIONS',                      _exp('Collect'),   { command => 'func'   } ],
+
+    [ '@Author::ETHER/Leftovers',       _exp('Leftovers'), {} ],
+
+    [ '@Author::ETHER/postlude',        _exp('Region'),    { region_name => 'postlude' } ],
+
+    [ '@Author::ETHER/Authors',         _exp('Authors'),   {} ],
+    [ '@Author::ETHER/Legal',           _exp('Legal'),     {} ],
+  )
+}
 
 1;
 __END__
@@ -14,19 +43,67 @@ __END__
 
 =head1 SYNOPSIS
 
-    use Pod::Weaver::PluginBundle::Author::ETHER;
+In your F<weaver.ini>:
 
-    ...
+    [@Author::ETHER]
+
+Or in your F<dist.ini>
+
+    [PodWeaver]
+    config_plugin = @Author::ETHER
 
 =head1 DESCRIPTION
 
-...
+=for stopwords optimizations
 
-=head1 FUNCTIONS/METHODS
+This is a L<Pod::Weaver> plugin bundle. It is I<approximately> equal to the
+following F<weaver.ini>, minus some optimizations:
 
-=head2 C<foo>
+    [@CorePrep]
 
-...
+    [-SingleEncoding]
+
+    [Name]
+    [Version]
+
+    [Region / prelude]
+
+    [Generic / SYNOPSIS]
+    [Generic / DESCRIPTION]
+    [Generic / OVERVIEW]
+
+    [Collect / ATTRIBUTES]
+    command = attr
+
+    [Collect / METHODS]
+    command = method
+
+    [Collect / FUNCTIONS]
+    command = func
+
+    [Leftovers]
+
+    [Region / postlude]
+
+    [Authors]
+
+    [Legal]
+
+This is also equivalent to:
+
+    [@Default]
+
+=head1 OPTIONS / OVERRIDES
+
+None at this time. (The bundle is never instantiated, so this doesn't seem to
+be possible without updates to L<Pod::Weaver>.)
+
+=head1 SEE ALSO
+
+=for :list
+L<Pod::Weaver>
+L<Pod::Weaver::PluginBundle::Default>
+L<Dist::Zilla::Plugin::PodWeaver>
 
 =head1 SUPPORT
 
@@ -36,13 +113,11 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Dist-Zilla-PluginBundle-Author-ETHER@rt.cpan.org|mailto:bug-Dist-Zilla-PluginBundle-Author-ETHER@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
+=head1 NAMING SCHEME
 
-...
+=for stopwords KENTNL
 
-=head1 SEE ALSO
-
-=for :list
-* L<foo>
+This distribution follows best practices for author-oriented plugin bundles; for more information,
+see L<KENTNL's distribution|Dist::Zilla::PluginBundle::Author::KENTNL/NAMING-SCHEME>.
 
 =cut
---
+++ t/12-podweaver-pluginbundle.t
@@ -0,0 +1,66 @@
+use strict;
+use warnings;
+
+use Test::More;
+use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
+use Test::DZil;
+use Test::Fatal;
+use Path::Tiny;
+
+# load this in advance, as we change directories between configuration and building
+use Pod::Weaver::PluginBundle::Author::ETHER;
+
+{
+    my $tzil = Builder->from_config(
+        { dist_root => 'does-not-exist' },
+        {
+            add_files => {
+                path(qw(source dist.ini)) => simple_ini(
+                    'GatherDir',
+                    [ PodWeaver => { config_plugin => '@Author::ETHER' } ],
+                ),
+                path(qw(source lib Foo.pm)) => <<FOO,
+package Foo;
+# ABSTRACT: Hello, this is foo
+
+1;
+=pod
+
+=cut
+FOO
+            },
+        },
+    );
+
+    $tzil->chrome->logger->set_debug(1);
+    is(
+        exception { $tzil->build },
+        undef,
+        'build proceeds normally',
+    );
+
+    my $module = $tzil->slurp_file('build/lib/Foo.pm');
+    my $version = $tzil->version;
+
+    isnt(index($module, <<POD), -1, 'module has woven the encoding, NAME and VERSION sections into pod');
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Foo - Hello, this is foo
+
+=head1 VERSION
+
+version $version
+
+POD
+
+    like($module, qr/^=head1 AUTHOR\n\n/m, 'module has woven the AUTHOR section into pod');
+
+    diag 'got log messages: ', explain $tzil->log_messages
+        if not Test::Builder->new->is_passing;
+}
+
+done_testing;

629e8359d7a7634dae6ad0ad85df1ea974a2ae51
Karen Etheridge <ether@cpan.org>
2015-11-11 20:56:34 -0800

    increment $VERSION after 0.108 release

--- lib/Dist/Zilla/PluginBundle/Author/ETHER.pm
+++ lib/Dist/Zilla/PluginBundle/Author/ETHER.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::PluginBundle::Author::ETHER;
 # KEYWORDS: author bundle distribution tool
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '0.108';
+our $VERSION = '0.109';
 
 use Moose;
 with

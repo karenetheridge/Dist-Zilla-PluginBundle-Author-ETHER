27b961894b185a41c8ffad14b7367b3a30692979
Karen Etheridge <ether@cpan.org>
2021-05-25 20:35:30 -0700

    freenode -> libera

--- lib/Pod/Weaver/PluginBundle/Author/ETHER.pm
+++ lib/Pod/Weaver/PluginBundle/Author/ETHER.pm
@@ -80,7 +80,7 @@ sub configure {
         : (),
 
     ($distmeta->{x_authority} // '') eq 'cpan:ETHER'
-    ? "I am also usually active on irc, as 'ether' at C<irc.perl.org> and C<irc.freenode.org>."
+    ? "I am also usually active on irc, as 'ether' at C<irc.perl.org> and C<irc.libera.chat>."
     : (),
 ) }}
 SUPPORT

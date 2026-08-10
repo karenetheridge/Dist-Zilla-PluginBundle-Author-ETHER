4f0b2ed1b0e6fc2170be230b9631563b84ec95bc
Karen Etheridge <ether@cpan.org>
2026-08-10 16:01:25 -0700

    increment $VERSION after 0.174 release

--- lib/Dist/Zilla/PluginBundle/Author/ETHER.pm
+++ lib/Dist/Zilla/PluginBundle/Author/ETHER.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::PluginBundle::Author::ETHER;
 # ABSTRACT: A plugin bundle for distributions built by ETHER
 # KEYWORDS: author bundle distribution tool
 
-our $VERSION = '0.174';
+our $VERSION = '0.175';
 
 use Moose;
 with
